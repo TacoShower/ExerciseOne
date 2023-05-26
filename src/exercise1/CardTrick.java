d3f553db0712064ccdebe3f2e61a26308d0d377f
Costa Soutos <costa.soutos@outlook.com>
2023-05-26 10:26:35 -0400

    Generate a random hand of 7 cards

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,12 +1,14 @@
 package exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Constantinos Soutos
+ * @author Constantinos Soutos May 26, 2023
  */
 public class CardTrick {
     
@@ -15,14 +17,11 @@ public class CardTrick {
         Card[] hand = new Card[7];
 
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            hand[i] = new Card();
+            hand[i].setValue((int)(Math.random() * 13) + 1);
+            hand[i].setSuit(Card.SUITS[(int)(Math.random() * 3) + 1]);
         }
-
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
